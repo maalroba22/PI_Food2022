cce731e781b60295746c99f5ee9d5e000af4521c
maalroba22 <maalroba22@gmail.com>
2022-07-16 08:49:50 -0500

    Modelos Realizados

--- api/prubas.js
+++
@@ -1,2 +0,0 @@
-const array = [];
-array.push(1);
--- api/src/models/Tipos.js
+++ api/src/models/Tipos.js
@@ -3,10 +3,11 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
+  sequelize.define('tipo', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
     },
   });
 };

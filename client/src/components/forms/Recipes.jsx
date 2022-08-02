f0ababd4c19268a84d728e98df4f609f33b9dfa5
maalroba22 <maalroba22@gmail.com>
2022-08-02 17:07:15 -0500

    Formulario Modificado

--- client/src/components/forms/Recipes.jsx
+++ client/src/components/forms/Recipes.jsx
@@ -2,10 +2,12 @@ import { React, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllDiet, postAddRecipes } from '../../redux/actions/index';
 import { Link, useHistory } from 'react-router-dom';
+import s from './Recipes.module.css';
+import order_image from '../../assets/img/recipes_formulario.png';
 
-import food from '../../assets/img/food1.svg';
+import logo_booton from '../../assets/img/logo_boottom.png';
 
-import './styles.css';
+/* import './styles.css'; */
 
 export default function Recipes() {
   const dispatch = useDispatch();
@@ -86,114 +88,141 @@ export default function Recipes() {
       <Link to="/home">
         <button>Volver</button>
       </Link>
-      <div className="container__forms">
-        <div className="container__logo">
-          <div>
-            {!input.image.trim() ? (
-              <img src={food} alt="no hay imagen" width="300px" />
-            ) : (
-              <img
-                src={input.image}
-                alt="no hay imagen"
-                width="300px"
-                height="270px"
-              />
-            )}
+      <div className={s.container}>
+        <h1>
+          <samp>Crear</samp>Recetas
+        </h1>
+        <div className={s.conatiner__main}>
+          <div className={s.order__image}>
+            <img src={order_image} alt="Imagen No Fount" />
           </div>
-        </div>
-        <div className="forms__info">
-          <h1>CREAR NUEVA RECETA</h1>
-          <form onSubmit={handleSubmit}>
-            <div className="input__text">
-              <input
-                className={errors.name && 'danger'}
-                type="text"
-                placeholder="Agregar un nombre de Receta:"
-                onChange={inputHandleChangue}
-                name="name"
-                value={input.name}
-              />
-            </div>
-            {errors.name && <p className="danger">{errors.name}</p>}
+          {/* ------------------Creacion del Formulario------------------ */}
 
-            <div>
-              <textarea
-                name="summary"
-                cols="47"
-                rows="3"
-                value={input.summary}
-                onChange={inputHandleChangue}
-                placeholder="Ingrese una Description de la Receta"
-              />
+          <div className="container__forms">
+            <div className="container__logo">
+              {/* <div>
+                {!input.image.trim() ? (
+                  <img src={food} alt="no hay imagen" width="300px" />
+                ) : (
+                  <img
+                    src={input.image}
+                    alt="no hay imagen"
+                    width="300px"
+                    height="270px"
+                  />
+                )}
+              </div> */}
             </div>
-            {errors.summary && <p className="danger">{errors.summary}</p>}
-            <div>
-              <span>{input.healthScore}</span>
-              <input
-                type="range"
-                name="healthScore"
-                min="1"
-                max="100"
-                value={input.healthScore}
-                onChange={rangeHhandleChangue}
-              />
-            </div>
+            <div className={s.forms__info}>
+              <h1>CREAR NUEVA RECETA</h1>
+              <form onSubmit={handleSubmit}>
+                <div className="input__text">
+                  <input
+                    className={errors.name && s.danger}
+                    type="text"
+                    placeholder="Agregar un nombre de Receta:"
+                    onChange={inputHandleChangue}
+                    name="name"
+                    value={input.name}
+                  />
+                </div>
+                {errors.name && <p className={s.danger}>{errors.name}</p>}
 
-            <div className="input__text">
-              <textarea
-                name="stepbyStep"
-                cols="47"
-                rows="3"
-                value={input.stepbyStep}
-                placeholder="Ingrese los pasos par crear la receta"
-                onChange={handleStep}
-              />
-            </div>
+                <div>
+                  <textarea
+                    name="summary"
+                    cols="40"
+                    rows="3"
+                    value={input.summary}
+                    onChange={inputHandleChangue}
+                    placeholder="Ingrese una Description de la Receta"
+                  />
+                </div>
+                {errors.summary && <p className={s.danger}>{errors.summary}</p>}
+                <div className={s.score}>
+                  <span>{input.healthScore}</span>
+                  <input
+                    type="range"
+                    name="healthScore"
+                    min="1"
+                    max="100"
+                    value={input.healthScore}
+                    onChange={rangeHhandleChangue}
+                  />
+                </div>
 
-            <div className="input__text">
-              <input
-                type="text"
-                name="image"
-                placeholder="ruta imagen"
-                value={input.image}
-                onChange={inputHandleChangue}
-              />
-            </div>
-            <div>
-              <select name="diet" onChange={(e) => selectHandleChangue(e)}>
-                {diets?.map((el) => (
-                  <option value={el.name}>{el.name}</option>
-                ))}
-              </select>
-              <ul>
-                {input.diet.map((el) => (
-                  <div className="dev__diet" key={el.id}>
-                    <li>{el}</li>
-                    {
-                      <span
-                        key={el.id}
-                        className="buton__x"
-                        onClick={() => handleDelete(el)}
-                      >
-                        x
-                      </span>
-                    }
+                <div className="input__text">
+                  <textarea
+                    name="stepbyStep"
+                    cols="40"
+                    rows="3"
+                    value={input.stepbyStep}
+                    placeholder="Ingrese los pasos par crear la receta"
+                    onChange={handleStep}
+                  />
+                </div>
+
+                <div className="input__text">
+                  <input
+                    type="text"
+                    name="image"
+                    placeholder="ruta imagen"
+                    value={input.image}
+                    onChange={inputHandleChangue}
+                  />
+                </div>
+                <div>
+                  <select name="diet" onChange={(e) => selectHandleChangue(e)}>
+                    {diets?.map((el) => (
+                      <option value={el.name}>{el.name}</option>
+                    ))}
+                  </select>
+                  <ul>
+                    {input.diet.map((el) => (
+                      <div className={s.diet} key={el.id}>
+                        <li>{el}</li>
+                        {
+                          <span
+                            key={el.id}
+                            className={s.buton__x}
+                            onClick={() => handleDelete(el)}
+                          >
+                            x
+                          </span>
+                        }
+                      </div>
+                    ))}
+                  </ul>
+                </div>
+
+                {!input.name || !input.summary ? (
+                  <div className={s.boton__add}>
+                    <input
+                      type="submit"
+                      value=" Guardar Recipes"
+                      className={s.boton__inactivo}
+                      disabled
+                    />
                   </div>
-                ))}
-              </ul>
+                ) : (
+                  <div className={s.boton__add}>
+                    <input
+                      type="submit"
+                      value=" Guardar Recipes"
+                      className={s.boton}
+                    />
+                  </div>
+                )}
+              </form>
+              <div>
+                <img
+                  src={logo_booton}
+                  alt="no foun"
+                  className={s.logo_bootom}
+                />
+              </div>
             </div>
-
-            {!input.name || !input.summary ? (
-              <input
-                type="submit"
-                value=" Guardar Recipes"
-                className="boton-inactivo"
-                disabled
-              />
-            ) : (
-              <input type="submit" value=" Guardar Recipes" className="boton" />
-            )}
-          </form>
+          </div>
         </div>
       </div>
     </>

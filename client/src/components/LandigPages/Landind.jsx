047e5e0c5ec0d35f27cf3f63a12a57cbf34415ff
maalroba22 <maalroba22@gmail.com>
2022-08-02 01:04:22 -0500

    diseño de LAnding

--- client/src/components/LandigPages/Landind.jsx
+++ client/src/components/LandigPages/Landind.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import chef from './img/chef1.png';
 import { Link } from 'react-router-dom';
+import './css/order.css';
 
 export default function Landing() {
   return (
-    <div>
-      <h1>Landing Page</h1>
-      <Link to="/Home">
-        <button>Ingresar..</button>
-      </Link>
-    </div>
+    <>
+      <div className="team">
+        <Link to="/home">
+          <h1>
+            App<span>Recipes</span>
+          </h1>
+        </Link>
+        <div className="team_box">
+          <div className="profile">
+            <img src={chef} alt="imagen" />
+
+            <div className="info">
+              <h2 className="name">Pi. Manuel Romero</h2>
+              <p className="bio">Diseño de App de Recetas de Comidas 2022</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
